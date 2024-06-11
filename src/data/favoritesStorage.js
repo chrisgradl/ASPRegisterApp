4f1482c1aabb890565726c937f55b35a55def0e5
cgradl <christian.gradl@icloud.com>
2024-06-11 16:47:26 +0200

    Revert "added favorit async storage"
    This reverts commit 1deb58bc7035fdde767d50e325aef59069659d02.

--- src/data/favoritesStorage.js
+++
@@ -1,9 +0,0 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-
-export function loadIsFavoriteById(id) {
-    return AsyncStorage.getItem(`favorites-${id}`).then(res => res === 'true');
-}
-
-export function saveFavoritById(id, value) {
-    return AsyncStorage.setItem(`favorites-${id}`, `${value}`);
-}
--- src/screens/ASPDetailScreen.js
+++ src/screens/ASPDetailScreen.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import {View, Button} from 'react-native';
+import {View} from 'react-native';
 import {Subheading} from 'react-native-paper';
 
 import ASPList from '../components/ASPList';
 import ASPDetailInfo from '../components/ASPDetailInfo';
 import {findDataByWirkstoff} from '../data/filterData';
-import { saveFavoritById, loadIsFavoriteById } from '../data/favoritesStorage';
 
 function ASPDetailScreen({ route }) {
+
     const item = route?.params?.item;
-
-    const [isFavorite, setIsFavorite] = React.useState()
-
-    React.useEffect(()=> {
-        loadIsFavoriteById(item.id).then(setIsFavorite)
-    }, [])
-
-    const toggleFavorite = async () => {
-        try {
-            await saveFavoritById(item.id, !isFavorite)
-            setIsFavorite(!isFavorite)
-          } catch (e) {
-            console.log(e);
-        }
-    }
-
-    const favoriteButtonText = isFavorite ? 'Remove Favorite' : 'Add to Favorites';
-
     const data = findDataByWirkstoff(item?.Wirkstoff)
         .filter(d => d.Name !== item.Name); // filter out item from screen
 
@@ -34,7 +16,6 @@ function ASPDetailScreen({ route }) {
         return (
             <React.Fragment>
                 <ASPDetailInfo item={item} />
-                <Button title={favoriteButtonText} onPress={toggleFavorite} />
                 <Subheading style={{ padding: 8 }}>Arzneimittel mit dem gleichen Wirkstoff</Subheading>
             </React.Fragment>
         )
